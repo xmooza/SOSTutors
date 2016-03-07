00e73efe471c9d2435ce79b1ec6c69aae79f3ce2
harry <hida0002@algonquinlive.com>
2016-03-06 23:57:01 -0500

    For later...

---
+++ SOSTutors/src/com/sos/servlets/StudentProfileServlet.java
@@ -0,0 +1,30 @@
+package com.sos.servlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sos.dao.StudentDAO;
+
+public class StudentProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{   	
+    	int tutorID = Integer.parseInt(request.getParameter("tutorID"));
+    	StudentDAO dao = new StudentDAO();
+    	
+    	try {
+    		request.setAttribute("student", dao.getStudentFromDatabaseById(tutorID));
+    		request.setAttribute("sessions", dao.getStudentSessions(tutorID));
+			request.setAttribute("comments", dao.getStudentComments(tutorID));
+			request.getRequestDispatcher("Profile.jsp").forward(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }  
+}
